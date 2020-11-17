226266b8731a0902f09ce7537f61a177c5325af5
elidioxg <elidioxg@gmail.com>
2020-11-17 20:31:44 -0300

    remove print from requests #27

--- binance_f/impl/restapirequestimpl.py
+++ binance_f/impl/restapirequestimpl.py
@@ -31,11 +31,7 @@ class RestApiRequestImpl(object):
         request.header.update({'Content-Type': 'application/json'})
         request.header.update({"X-MBX-APIKEY": self.__api_key})
         request.url = url + "?" + builder.build_url()
-         # For develop
-        print("====== Request ======")
-        print(request)
-        PrintMix.print_data(request)
-        print("=====================")
+
         return request
 
     def __create_request_by_post_with_signature(self, url, builder):
@@ -49,11 +45,7 @@ class RestApiRequestImpl(object):
         request.header.update({"X-MBX-APIKEY": self.__api_key})
         request.post_body = builder.post_map
         request.url = url + "?" + builder.build_url()
-        # For develop
-        print("====== Request ======")
-        print(request)
-        PrintMix.print_data(request)
-        print("=====================")
+
         return request
 
     def __create_request_by_delete_with_signature(self, url, builder):
@@ -66,11 +58,7 @@ class RestApiRequestImpl(object):
         request.header.update({'Content-Type': 'application/json'})
         request.header.update({"X-MBX-APIKEY": self.__api_key})
         request.url = url + "?" + builder.build_url()
-        # For develop
-        print("====== Request ======")
-        print(request)
-        PrintMix.print_data(request)
-        print("=====================")
+
         return request
 
     def __create_request_by_get_with_signature(self, url, builder):
@@ -83,11 +71,7 @@ class RestApiRequestImpl(object):
         request.header.update({"Content-Type": "application/x-www-form-urlencoded"})
         request.header.update({"X-MBX-APIKEY": self.__api_key})
         request.url = url + "?" + builder.build_url()
-        # For develop
-        print("====== Request ======")
-        print(request)
-        PrintMix.print_data(request)
-        print("=====================")
+
         return request
 
     def __create_request_by_put_with_signature(self, url, builder):
@@ -100,11 +84,7 @@ class RestApiRequestImpl(object):
         request.header.update({'Content-Type': 'application/json'})
         request.header.update({"X-MBX-APIKEY": self.__api_key})
         request.url = url + "?" + builder.build_url()
-        # For develop
-        print("====== Request ======")
-        print(request)
-        PrintMix.print_data(request)
-        print("=====================")
+
         return request
         
     def get_servertime(self):
